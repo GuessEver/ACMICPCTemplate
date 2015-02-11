cfb80aee33334b5cdc1883e0e6a7d85d989f971a
GuessEver <jiangzh777@163.com>
2015-02-11 22:44:33 +0800

    update KMP

--- string/kmp.cpp
+++ string/kmp.cpp
@@ -1,26 +1,38 @@
-void getnext(char *t)
+#include <cstdio>
+#include <cstring>
+// POJ 3461 : Count the number of t occurrences in s
+char s[1000000+10], t[1000000+10];
+int lens, lent, next[1000000+10];
+
+void getNext(char *t, int len, int *Next)
 {
-	memset(next, 0, sizeof(next));
-	next[0] = -1;
-	int j = 0, k = -1;
-	int len = strlen(t);
-	while(j < len)
+	memset(Next, 0, sizeof(Next)); Next[0] = -1;
+	for(int j = 0, k = -1; j < len; )
 	{
-		if(k == -1 || t[j] == t[k]) next[++j] = ++k;
-		else k = next[k];
+		if(k == -1 || t[j] == t[k]) Next[++j] = ++k;
+		else k = Next[k];
 	}
 }
-void kmp()
+int kmp(char *s, int lens, char *t, int lent)
 {
-	getnext();
-	int lens = strlen(s), lent = strlen(t);
-	int i = 0, j = 0;
-	while(i < lens)
+	int res = 0;
+	getNext(t, lent, next);
+	for(int i = 0, j = 0; i < lens; )
 	{
-		if(j == -1 || s[i] == t[j]) i++, j++;
+		if(j == -1 || s[i] == t[j]) { i++; j++; }
 		else j = next[j];
-		if(j == lent) return j - lent;
+		if(j == lent) res++; // Bingo! [pos = j - lent]
 	}
-	return -1;
+	return res;
 }
 
+int main()
+{
+	int T; scanf("%d", &T);
+	while(T--)
+	{
+		scanf("%s%s", t, s);
+		printf("%d\n", kmp(s, strlen(s), t, strlen(t)));
+	}
+	return 0;
+}
