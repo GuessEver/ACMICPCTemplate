05ee4c272494a9aaec751300ca6db55da468c9e0
GuessEver <jiangzh777@163.com>
2015-05-23 17:12:36 +0800

    update mistakes

--- graph/isap.cpp
+++ graph/isap.cpp
@@ -1,5 +1,12 @@
-//   h[x] :   点 x 在第 h[x] 层
-//   v[k] :   第 k 层有 v[k] 个点 
+#include <cstdio>
+#include <algorithm>
+
+const int N = 200 + 10;
+
+int n, m, g[N][N];
+int v[N], h[N];
+int S, T;
+
 int sap(int x, int flow)
 {
 	if(x == n) return flow;
@@ -7,7 +14,7 @@ int sap(int x, int flow)
 	for(int i = S; i <= T; i++)
 		if(g[x][i] && h[x] == h[i] + 1)
 		{
-			int t = sap(i, min(g[x][i], flow - res));
+			int t = sap(i, std::min(g[x][i], flow - res));
 			res += t; g[x][i] -= t; g[i][x] += t;
 			if(res == flow) return res;
 			if(h[S] >= T) return res;
@@ -17,11 +24,20 @@ int sap(int x, int flow)
 	++v[++h[x]];
 	return res;
 }
+
 int main()
 {
-	v[0] = T;
+	scanf("%d%d", &m, &n); // m = number of edges, n = number of points
+	for(int i = 1; i <= m; i++)
+	{
+		int x, y, z;
+		scanf("%d%d%d", &x, &y, &z);
+		g[x][y] += z;
+	}
+	v[0] = T; S = 1; T = n;
 	int maxflow = 0;
-	while(h[S] < T) maxflow += sap(1, inf);
-	reutrn 0;
+	while(h[S] < T) maxflow += sap(1, 0x3f3f3f3f);
+	printf("%d\n", maxflow);
+	return 0;
 }
 
--- graph/prim.cpp
+++ graph/prim.cpp
@@ -1,18 +1,19 @@
-void prime()
+int prim()
 {
-	memset(dist, 0, sizeof(dist));
+	memset(dist, 0x3f, sizeof(dist));
+	dist[1] = 0; Q.push(make_pair(0, 1));
 	int res = 0;
 	while(!Q.empty())
 	{
-		int x = Q.top().second;
-		if(done[x]) {Q.pop(); continue;}
-		res += Q.top().first;
-		Q.pop();
+		int x = Q.top().second; Q.pop();
+		if(done[x]) continue;
+		res += dist[x]; done[x] = 1;
 		for(Link p = head[x]; p; p = p->next)
 			if(dist[p->y] > p->z)
 			{
 				dist[p->y] = p->z;
-				Q.push(make_pair(dist[p->y], p->y));
+				Q.push(make_pair(-dist[p->y], p->y));
 			}
 	}
+	return res;
 }
