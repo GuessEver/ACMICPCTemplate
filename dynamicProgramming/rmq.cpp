d970a19386bb5c3bd36e20ebbc1c866e142f7e82
GuessEver <jiangzh777@163.com>
2015-05-28 14:09:46 +0800

    update RMQ

--- dynamicProgramming/rmq.cpp
+++ dynamicProgramming/rmq.cpp
@@ -4,10 +4,10 @@ void init_RMQ(int *_orig) // [1, n]
 	for(int i = 1; i <= n; i++) _rmq[i][0] = _orig[i];
 	for(int j = 1; j <= log(1.0 * n) / log(2.0); j++)
 		for(int i = 1; i <= n + 1 - (1 << j); i++)
-			_rmq[i][j] = std::max(_rmq[i][j-1], _rmq[i+(1 << (j-1))][j-1]);
+			_rmq[i][j] = std::max(_rmq[i][j-1], _rmq[i+(1<<(j-1))][j-1]);
 }
 int query(int l, int r) // max{x E [l, r]}
 {
 	int k = log(r - l + 1.0) / log(2.0);
-	return std::max(_rmq[l][k], _rmq[r-(1 << k) + 1][k]);
+	return std::max(_rmq[l][k], _rmq[r-(1<<k)+1][k]);
 }
