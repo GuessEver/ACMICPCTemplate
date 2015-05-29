f72b3d162e6c5c8ec5e2a9d78c592899abc38d5a
GuessEver <jiangzh777@163.com>
2015-05-29 20:18:01 +0800

    update LCA_doublingAlgorithm

--- dynamicProgramming/rmq.cpp
+++ dynamicProgramming/rmq.cpp
@@ -6,7 +6,7 @@ void init_RMQ(int *_orig) // [1, n]
 		for(int i = 1; i <= n + 1 - (1 << j); i++)
 			_rmq[i][j] = std::max(_rmq[i][j-1], _rmq[i+(1<<(j-1))][j-1]);
 }
-int query(int l, int r) // max{x E [l, r]}
+int query_RMQ(int l, int r) // max{x E [l, r]}
 {
 	int k = log(r - l + 1.0) / log(2.0);
 	return std::max(_rmq[l][k], _rmq[r-(1<<k)+1][k]);
