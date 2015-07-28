c3c75f27f9a0e885118de21be7793cb5a8fc325f
GuessEver <jiangzh777@163.com>
2015-07-29 03:09:06 +0800

    update treap

--- datastructure/treap(Array).cpp
+++ datastructure/treap(Array).cpp
@@ -1,105 +1,73 @@
-#include <cstdio>
-#include <cstdlib>
-#include <ctime>
-
-const int N = 100000 + 10;
-
-int m, Limit;
-int L[N], R[N], S[N], fix[N], key[N];
-int root, total, leave;
-
-void rotate_left(int &p)
-{
-	int tmp = R[p];
-	R[p] = L[tmp];
-	int zsize = S[L[tmp]];
-	S[p] = S[p] - S[tmp] + zsize;
-	L[tmp] = p;
-	S[tmp] = S[tmp] - zsize + S[p];
-	p = tmp;
-}
-void rotate_right(int &p)
-{
-	int tmp = L[p];
-	L[p] = R[tmp];
-	int zsize = S[R[tmp]];
-	S[p] = S[p] - S[tmp] + zsize;
-	R[tmp] = p;
-	S[tmp] = S[tmp] - zsize + S[p];
-	p = tmp;
-}
-
-void insert(int &p, int x)
-{
-	if(!p)
+struct treap {
+	int L[N*20], R[N*20], S[N*20], fix[N*20], key[N*20];
+	int root, total;
+	void rotate_left(int &p)
 	{
-		p = ++total;
-		L[p] = R[p] = 0;
-		S[p] = 1;
-		fix[p] = rand();
-		key[p] = x;
-		return;
+		int tmp = R[p];
+		R[p] = L[tmp];
+		int zsize = S[L[tmp]];
+		S[p] = S[p] - S[tmp] + zsize;
+		L[tmp] = p;
+		S[tmp] = S[tmp] - zsize + S[p];
+		p = tmp;
 	}
-	S[p]++;
-	if(x < key[p])
+	void rotate_right(int &p)
 	{
-		insert(L[p], x);
-		if(fix[L[p]] > fix[p]) rotate_right(p);
+		int tmp = L[p];
+		L[p] = R[tmp];
+		int zsize = S[R[tmp]];
+		S[p] = S[p] - S[tmp] + zsize;
+		R[tmp] = p;
+		S[tmp] = S[tmp] - zsize + S[p];
+		p = tmp;
 	}
-	else {
-		insert(R[p], x);
-		if(fix[R[p]] > fix[p]) rotate_left(p);
-	}
-}
-
-void remove(int &p, int limit)
-{
-	if(!p) return;
-	if(key[p] < limit)
+	void Insert(int &p, int x)
 	{
-		leave += S[L[p]] + 1;
-		p = R[p];
-		remove(p, limit);
-	}
-	else{
-		remove(L[p], limit);
-		S[p] = S[L[p]] + S[R[p]] + 1;
-	}
-}
-
-int kth(int &p, int k)
-{
-	if(k <= S[L[p]]) return kth(L[p], k);
-	else if(k == S[L[p]] + 1) return key[p];
-	else return kth(R[p], k - S[L[p]] - 1);
-}
-
-int main()
-{
-	srand(time(0));
-	scanf("%d%d", &m, &Limit);
-	int delta = 0;
-	while(m--)
-	{
-		char op; int x;
-		scanf(" %c%d", &op, &x);
-		if(op == 'I')
+		if(!p)
 		{
-			if(x < Limit) continue;
-			insert(root, x - delta);
+			p = ++total;
+			L[p] = R[p] = 0;
+			S[p] = 1;
+			fix[p] = rand();
+			key[p] = x;
+			return;
 		}
-		else if(op == 'A') delta += x;
-		else if(op == 'S')
+		S[p]++;
+		if(x < key[p])
 		{
-			delta -= x;
-			remove(root, Limit - delta);
+			Insert(L[p], x);
+			if(fix[L[p]] > fix[p]) rotate_right(p);
 		}
 		else {
-			x = S[root] - x + 1;
-			if(x <= 0) puts("-1");
-			else printf("%d\n", kth(root, x) + delta);
+			Insert(R[p], x);
+			if(fix[R[p]] > fix[p]) rotate_left(p);
 		}
 	}
-	printf("%d\n", leave);
-	return 0;
-}
+	int Delete_min(int &p)
+	{
+		S[p]--;
+		if(!L[p])
+		{
+			int value = key[p];
+			p = R[p];
+			return value;
+		}
+		else return Delete_min(L[p]);
+	}
+	void Delete(int &p, int x)
+	{
+		if(!p) return;
+		S[p]--;
+		if(x < key[p]) Delete(L[p], x);
+		else if(x > key[p]) Delete(R[p], x);
+		else {
+			if(!L[p] && !R[p]) p = 0;
+			else if(!L[p] || !R[p])
+			{
+				if(!L[p]) p = R[p];
+				else p = L[p];
+			}
+			else key[p] = Delete_min(R[p]);
+		}
+	}
+};
