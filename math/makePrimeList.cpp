38df771baa05b6bd6cb80eab1c71feb2ca38605e
GuessEver <jiangzh777@163.com>
2015-08-11 21:19:36 +0800

    update

--- math/makePrimeList.cpp
+++ math/makePrimeList.cpp
@@ -1,12 +1,17 @@
-void make_prime_list(int maxp) // O(2*N)
-{
-	for(int i = 2; i <= maxp; i++)
+namespace prime{
+	const int N = 10000000 + 10;
+	int pri[N], h[N], Cnt;
+	void make(int maxp) // O(2*N)
 	{
-		if(!h[i]) pri[l++] = i;
-		for(int j = 0; j < l && pri[j] <= maxp / i; j++)
+		for(int i = 2; i <= maxp; i++)
 		{
-			h[i * pri[j]] = true;
-			if(i % pri[j] == 0) break;
+			if(!h[i]) pri[Cnt++] = i;
+			for(int j = 0; j < Cnt && pri[j] <= maxp / i; j++)
+			{
+				h[i * pri[j]] = true;
+				if(i % pri[j] == 0) break;
+			}
 		}
 	}
 }
+
